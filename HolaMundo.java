33b0980fe5654df79bd5dd741e2f45c4a0510f1b
JordyAMT <yordiam12gonzales@gmail.com>
2025-06-20 15:19:48 -0600

    Conflicto resuelto

--- HolaMundo.java
+++ HolaMundo.java
@@ -10,11 +10,10 @@ public class HolaMundo {
         System.out.println("La suma de " + a + " + " + b + " es: " + suma);
         System.out.println("La multiplicación de " + a + " * " + b + " es: " + (a * b));
         System.out.println("Hola, soy Jordy Martinez , Carné 2025-0244U");
-<<<<<<< HEAD
-        System.out.println("Hola desde master");
-=======
-        System.out.println("Hola desde feature-saludoprofe");
->>>>>>> feature-saludoprofe
+
+        System.out.println("Hola desde master y feature-saludoprofe");
+
+
 
 
     }
